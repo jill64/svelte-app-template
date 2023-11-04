2f3b780df7df8bdc201d206993571ea5a7e20b8a
jill64 <intents.turrets0h@icloud.com>
2023-11-04 23:25:20 +0900

    Refactor i18n and add OGP support

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,10 +1,11 @@
 import { PUBLIC_SENTRY_DSN } from '$env/static/public'
-import { attach } from '$lib/i18n'
+import { attach as lang_attach } from '$lib/i18n'
 import { serverInit } from '@jill64/sentry-sveltekit-cloudflare'
 import { onRender } from '@jill64/svelte-dark-theme'
+import { attach as ogp_attach } from '@jill64/svelte-ogp'
 import { sequence } from '@sveltejs/kit/hooks'
 
 const { onHandle, onError } = serverInit(PUBLIC_SENTRY_DSN)
 
-export const handle = onHandle(sequence(attach, onRender()))
+export const handle = onHandle(sequence(lang_attach, onRender(), ogp_attach))
 export const handleError = onError()
