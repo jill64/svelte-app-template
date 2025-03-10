d49ec52546288ed40a04449b3265b1eaca4bca35
jill64 <contact@jill64.dev>
2025-03-11 00:24:12 +0900

    fix: update test imports to use bun:test and clean up vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  // @ts-expect-error TODO: Fix this
   plugins: [sveltekit()],
   ssr: {
     noExternal: ['@jill64/sentry-sveltekit-cloudflare']
